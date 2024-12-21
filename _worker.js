9a35f71b0449f92af9700e9ce9bb0e5eef29f59d
GGFDEY <asddd@pissmail.com>
2024-12-21 20:45:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '41035ee5-7d0e-401e-9084-2d205b569fb3';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
